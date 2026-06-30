e102acb1c70156576681f8863562d872c94e05a8
Marcio Fernandes <marcio.fernandes@verdanatech.com>
2026-06-30 08:48:59 -0300

    Correction Formcreator Version

--- setup.php
+++ setup.php
@@ -339,7 +339,7 @@ function plugin_formcreator_hook(): void {
             Session::getCurrentInterface() == 'helpdesk') {
 
          // Add specific JavaScript
-         $PLUGIN_HOOKS[Hooks::ADD_JAVASCRIPT]['formcreator'][] = 'js/scripts.jsv=1.0.0';
+         $PLUGIN_HOOKS[Hooks::ADD_JAVASCRIPT]['formcreator'][] = 'js/scripts.js';
       }
 
       if (isset($_SESSION['glpiactiveentities_string'])) {
